93f314c1bd7d331af70f6e917e7f5a34005f61d9
Dandara Soares <danda.dolls@gmail.com>
2023-11-22 16:30:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SITE-JAVA-ADV
+ Aplicativo em desenvolvimento no curso de JAVA no SENAC-RJ
---
+++ src/main/java/Controller/AdvogadoController.java
@@ -0,0 +1,109 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Advogado;
+import model.AdvogadoDao;
+
+/**
+ * Servlet implementation class AdvogadoController
+ */
+@WebServlet({ "/AdvogadoController", "/novoadvogado", "/buscaadvogados","/apagaradvogado" })
+public class AdvogadoController extends HttpServlet {
+
+	Advogado adv = new Advogado();
+	AdvogadoDao daoadv = new AdvogadoDao();
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AdvogadoController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String acao = request.getServletPath();
+		
+		if (acao.equals("/novoadvogado")) {
+			EnviaDados(request, response);
+		} else if (acao.equals("/buscaadvogados")) {
+			BuscaDados(request, response);
+		} else if (acao.equals("/apagaradvogado")) {
+			ApagaDados (request,response);
+		}
+	}
+
+	protected void EnviaDados(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		adv.setOab(request.getParameter("oab"));
+		adv.setNome(request.getParameter("nome"));
+		adv.setTelefone(request.getParameter("telefone"));
+		daoadv.Salvar(adv);
+		request.setAttribute("success", "Cadastro Concluído!");
+		request.getRequestDispatcher("buscaadvogados").forward(request, response);
+	}
+	
+	
+	
+
+	protected void BuscaDados(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ArrayList<Advogado> lista = daoadv.Listar();
+		
+		String success = (String) request.getAttribute("success");
+		
+		if(success != null) {
+			request.setAttribute ("success", success);
+		}
+		
+		request.setAttribute("advogados", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("RelAdvogado.jsp");
+		rd.forward(request, response);
+
+	}
+	
+	
+	protected void ApagaDados(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String id = request.getParameter("id");
+		daoadv.Apagar(id);
+		request.setAttribute("success","Advogado apagado com sucesso");
+		request.getRequestDispatcher("buscaadvogados").forward(request,response);
+		
+		System.out.println (request.getParameter("id"));
+		
+	}
+		
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
